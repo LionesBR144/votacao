3837adbd440e59f55c7c59c2be021e7bd2cba362
LionesBR144 <isaacleonmarques11@gmail.com>
2025-02-06 14:01:19 -0300

    Modificações efetuadas em package-lock.json, package.json e 6 outros arquivos...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import  localePt  from "@angular/common/locales/pt";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { registerLocaleData } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -10,9 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
